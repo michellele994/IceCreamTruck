53623a899cd7dd1d3d3e1d9033a49a79d83aad7c
michellele994 <michellele@email.arizona.edu>
2018-07-21 15:58:24 -0700

    Got a square to move up and down in an interval

--- icecreamtruck.js
+++ icecreamtruck.js
@@ -1,4 +1,25 @@
-var canvas = document.getElementById("truck");
-var ctx = canvas.getContext("2d");
-ctx.fillStyle = "#FF0000";
-ctx.fillRect(0, 0, 150, 75);
+var xPos = 0;
+var yPos = 0
+setInterval(function () {
+    if (yPos == 0) {
+        yPos = 50;
+        setUp(xPos, yPos)
+    }
+    else {
+        yPos = 0;
+        setUp(xPos, yPos)
+    }
+
+}, 800)
+
+function setUp(xPos, yPos) {
+    var recanvas = document.getElementById("truck");
+    var rectx = recanvas.getContext("2d");
+    rectx.fillStyle = "#FFFFFF";
+    rectx.fillRect(0, 0, 500, 500);
+
+    var canvas = document.getElementById("truck");
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = "#FF0000";
+    ctx.fillRect(xPos, yPos, 150, 75);
+}
