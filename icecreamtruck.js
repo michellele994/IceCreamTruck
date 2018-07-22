c951302678c7411768c6585eb818245401750776
michellele994 <michellele@email.arizona.edu>
2018-07-22 15:00:38 -0700

    Added windows! Played with fill. Will clean up code later

--- icecreamtruck.js
+++ icecreamtruck.js
@@ -19,31 +19,28 @@ function setUp(xPos, yPos) {
     drawTruck(canvas, ctx);
 }
 function drawCanvas(canvas, ctx) {
-    ctx.fillStyle = "lightyellow";
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "transparent";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 function drawTruck(canvas, ctx) {
-    ctx.lineWidth = "3";
+    ctx.lineWidth = "2";
     ctx.strokeStyle = "#777";
-    //starting at top left rounded corner going clockwise
     ctx.beginPath();
     ctx.arc(xPos + 10, yPos + 10, 10, 1.2 * Math.PI, 1.5 * Math.PI);
-    //top right rounded corner
+    //Top right rounded corner
     ctx.arc(xPos + (canvas.width * (2 / 3)) - 10, yPos + 10, 10, 1.5 * Math.PI, 0 * Math.PI);
-    //bottom right rounded corner
+    //Bottom right rounded corner
     ctx.arc(xPos + (canvas.width * (2 / 3)) - 10, yPos + (canvas.height * (3 / 5)) - 10, 10, 0, 0.5 * Math.PI);
-    //bottom left rounded corner
+    //Bottom left rounded corner
     ctx.arc(xPos - (canvas.width * (1 / 6)), yPos + (canvas.height * (3 / 5)) - 10, 10, 0.5 * Math.PI, Math.PI);
-    //engine
+    //Engine
     ctx.arc(xPos - (canvas.width * (1 / 6)), yPos + (canvas.height * (1 / 3)), 10, Math.PI, 1.4 * Math.PI);
     ctx.lineTo(xPos - (canvas.width * (1 / 15)) - 10, yPos + (canvas.height * (1 / 4)) - 10);
     ctx.closePath();
-    ctx.stroke();
-
-    // Filling in the truck color
     ctx.fillStyle = "white";
     ctx.fill();
-
+    ctx.stroke();
     //Front tire
     ctx.beginPath()
     ctx.lineWidth = "45";
@@ -51,16 +48,36 @@ function drawTruck(canvas, ctx) {
     ctx.arc((canvas.width * (1 / 4)) - (canvas.width * (1 / 25)), (canvas.height * (1 / 4)) + (canvas.height * (3 / 5)), 30, 0, 2 * Math.PI);
     ctx.closePath();
     ctx.stroke();
-
     //Back tire
-    ctx.beginPath()
-    ctx.lineWidth = "45";
-    ctx.strokeStyle = "#777";
+    ctx.beginPath();
     ctx.arc((canvas.width * (1 / 4)) + (canvas.width * (5 / 9)), (canvas.height * (1 / 4)) + (canvas.height * (3 / 5)), 30, 0, 2 * Math.PI);
     ctx.closePath();
     ctx.stroke();
 
-    // ctx.moveTo(xPos, yPos);
-    // ctx.arc(300, 300, 30, 0, 2 * Math.PI);
-    // ctx.stroke();
+    drawTruckDetails(canvas, ctx);
 }
+function drawTruckDetails(canvas, ctx) {
+    ctx.lineWidth = "2"
+    ctx.strokeStyle = "#CBCBCB";
+    //Driver Window
+    ctx.beginPath();
+    ctx.moveTo(xPos + 10, yPos + 20);
+    ctx.lineTo(xPos + (canvas.width * (1 / 10)), yPos + 20);
+    ctx.lineTo(xPos + (canvas.width * (1 / 10)), yPos + (canvas.height * (1 / 4)) - 10);
+    ctx.lineTo(xPos - (canvas.width * (1 / 15)) + 10, yPos + (canvas.height * (1 / 4)) - 10);
+    ctx.closePath();
+    ctx.fillStyle = "lightblue";
+    ctx.fill();
+    ctx.stroke();
+
+    //Ice Cream Window
+    ctx.beginPath();
+    ctx.rect(xPos + (canvas.width * (1 / 7)), yPos + 20, (canvas.width * (3 / 7)), (canvas.height * (1 / 3)))
+    ctx.fillStyle = "#DDD";
+    ctx.fill();
+    ctx.closePath();
+    ctx.stroke();
+
+    //Ice Cream Window Curtains
+};
+
